90b712ae5f35a7c930598b5edfdaf8f5bf29ebdf
Timothy J Moore <tjstretchalot@gmail.com>
2013-05-20 11:26:12 -0400

    Use the enum like an enum
    Enums are first-class objects, meant to replace these ugly if
    statements. Use them like that!

--- src/code/husky/Database.java
+++ src/code/husky/Database.java
@@ -16,35 +16,11 @@ public abstract class Database
 
   protected Statements getStatement(String query) {
     String trimmedQuery = query.trim();
-    if (trimmedQuery.substring(0, 6).equalsIgnoreCase("SELECT"))
-      return Statements.SELECT;
-    if (trimmedQuery.substring(0, 6).equalsIgnoreCase("INSERT"))
-      return Statements.INSERT;
-    if (trimmedQuery.substring(0, 6).equalsIgnoreCase("UPDATE"))
-      return Statements.UPDATE;
-    if (trimmedQuery.substring(0, 6).equalsIgnoreCase("DELETE"))
-      return Statements.DELETE;
-    if (trimmedQuery.substring(0, 6).equalsIgnoreCase("CREATE"))
-      return Statements.CREATE;
-    if (trimmedQuery.substring(0, 5).equalsIgnoreCase("ALTER"))
-      return Statements.ALTER;
-    if (trimmedQuery.substring(0, 4).equalsIgnoreCase("DROP"))
-      return Statements.DROP;
-    if (trimmedQuery.substring(0, 8).equalsIgnoreCase("TRUNCATE"))
-      return Statements.TRUNCATE;
-    if (trimmedQuery.substring(0, 6).equalsIgnoreCase("RENAME"))
-      return Statements.RENAME;
-    if (trimmedQuery.substring(0, 2).equalsIgnoreCase("DO"))
-      return Statements.DO;
-    if (trimmedQuery.substring(0, 7).equalsIgnoreCase("REPLACE"))
-      return Statements.REPLACE;
-    if (trimmedQuery.substring(0, 4).equalsIgnoreCase("LOAD"))
-      return Statements.LOAD;
-    if (trimmedQuery.substring(0, 7).equalsIgnoreCase("HANDLER"))
-      return Statements.HANDLER;
-    if (trimmedQuery.substring(0, 4).equalsIgnoreCase("CALL")) {
-      return Statements.CALL;
-    }
+    Statements[] allStatements = Statements.values();
+	for(Statement statement : allStatements) {
+		if(trimmedQuery.startsWith(statement.name()))
+			return statement;
+	}
     return Statements.SELECT;
   }
 
